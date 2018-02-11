7654e4ce0cd05b1df19c94444bac2172766633a6
Tadaboody <tomerpet@gmail.com>
2018-02-11 15:17:49 +0200

    Add backwards compatibility
    Instead of creating/copying the button DOM node I clone it from the first
    button in its section. this creates the problem that it opens the same
    link as the first button, even though I reset its onClick method.

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+chrome.runtime.sendMessage({ type: "course_name", course_name: getCourseName() });
 Array.from(document.getElementsByClassName("section img-text")).forEach(
     (ul, index, ulArray) => {
         if (index === 0) {//first section is intro/sylabus
@@ -11,12 +12,9 @@ Array.from(document.getElementsByClassName("section img-text")).forEach(
         ul.insertBefore(download_button, ul.firstChild);
     }
 )
-chrome.runtime.sendMessage({ type: "course_name", course_name: getCourseName() });
 function getCourseName()
 {
-    var nav_links = document.getElementsByClassName("breadcrumb-item");
-    var course_link = nav_links[nav_links.length - 1];
-    let full_course_name =  course_link.getElementsByTagName("a")[0].title;//includes course number
+    let full_course_name =  document.getElementsByTagName("h1")[0].textContent;//includes course number
     return /(\D+)/g.exec(full_course_name)[1].trim();//TODO: add option to keep the course number
 }
 
@@ -63,23 +61,33 @@ function create_dl_button(section,section_name)
     const button_icon = "https://mw5.haifa.ac.il/theme/image.php/boost/core/1517353424/f/archive-24";
     const button_text = "הורד את כל הפרק";
     const button_function = download_section_factory(section, section_name);
+    const desc_text = "Mass Moodle Downloader";
 
-    var download_button = document.createElement("li");
-    download_button.setAttribute("class", "activity resource modtype_resource ")
-    const button_html = '<div><div class="mod-indent-outer"><div class="mod-indent"></div><div><div class="activityinstance"><a class="" onclick="" href="https://mw5.haifa.ac.il/mod/resource/view.php?id=11679"><img src="https://mw5.haifa.ac.il/theme/image.php/boost/core/1517353424/f/pdf-24" class="iconlarge activityicon" alt=" " role="presentation"><span class="instancename">הרצאה 1 - מבוא<span class="accesshide "> קובץ</span></span></a> <span class="resourcelinkdetails">מסמך PDF</span></div></div></div></div>'
-    download_button.innerHTML = button_html;
-
+    var download_button = section.firstChild.cloneNode(true);
+    download_button.setAttribute("id","");
     var link = download_button.getElementsByTagName("a")[0];
+    link.setAttribute("onclick","");
     link.onclick = button_function;
-    link.href = ""//Legal, makes page refresh on redirect
+    // link.href = ""//Legal, makes page refresh on redirect
 
     var icon = download_button.getElementsByTagName("img")[0];
     icon.src = button_icon; //todo: custom icon
     var text = download_button.getElementsByClassName("instancename")[0];
     text.textContent = button_text;
 
-    var desc = download_button.getElementsByClassName("resourcelinkdetails")[0];
-    desc.textContent = "Mass Moodle Downloader"
-    
+    var activity_instance = download_button.getElementsByClassName("activityinstance")[0];
+    let desc_list = activity_instance.getElementsByClassName("resourcelinkdetails");
+    var desc = {};
+    if(desc_list.length == 0)
+    {
+        desc = document.createElement("span");
+        desc.setAttribute("class", "resourcelinkdetails");
+    }
+    else {
+        desc = desc_list[0]
+    }
+    desc.textContent = desc_text;
+    activity_instance.appendChild(desc);
+
     return download_button;
 }
