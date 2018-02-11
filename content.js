ee7a7946dc9625314fb603ac3a4ff53cf03b62f9
Tadaboody <tomerpet@gmail.com>
2018-02-11 03:30:24 +0200

    Simplify button building
    I was not going to do this at first because this didn't seem it was going
    to be readable but it actually removes a lot of the noise that was
    surrounding building the buttons and only shows the important parts.
    
    I'd like to be able to import button_html directly but that probably
    requires a build system which is really out of scope for what I'm doing
    here.
    
    I also deleted an unused function that got away - whoops!

--- content.js
+++ content.js
@@ -43,10 +43,6 @@ function download_section_factory(section,section_name)
     return download_section;
 }
 
-function file_ext(image)
-{
-    known_ext = []
-}
 function download_file(url,filename)
 {
     chrome.runtime.sendMessage({ type: "download", url: url }, function (response) { console.log("BYE!") });
@@ -56,43 +52,22 @@ function create_dl_button(section,section_name)
 {
     const button_icon = "https://mw5.haifa.ac.il/theme/image.php/boost/core/1517353424/f/archive-24";
     const button_text = "הורד את כל הפרק";
-    const button_function = download_section_factory(section,section_name);
-    //horrible javascript re-creating the html and styling of a moodle button
+    const button_function = download_section_factory(section, section_name);
+
     var download_button = document.createElement("li");
-    download_button.setAttribute("class","activity resource modtype_resource ")
-        var div = document.createElement("div");
-            var mod_indent_outer = document.createElement("div");
-            mod_indent_outer.setAttribute("class","mod-indent-outer");
-                var mod_indent = document.createElement("div");
-                mod_indent.setAttribute("class","mod-indent");
-            mod_indent_outer.appendChild(mod_indent);
+    download_button.setAttribute("class", "activity resource modtype_resource ")
+    const button_html = '<div><div class="mod-indent-outer"><div class="mod-indent"></div><div><div class="activityinstance"><a class="" onclick="" href="https://mw5.haifa.ac.il/mod/resource/view.php?id=11679"><img src="https://mw5.haifa.ac.il/theme/image.php/boost/core/1517353424/f/pdf-24" class="iconlarge activityicon" alt=" " role="presentation"><span class="instancename">הרצאה 1 - מבוא<span class="accesshide "> קובץ</span></span></a> <span class="resourcelinkdetails">מסמך PDF</span></div></div></div></div>'
+    download_button.innerHTML = button_html;
 
-            var inner_div = document.createElement("div");
-                var activity_instance = document.createElement("div")
-                activity_instance.setAttribute("class","activity");
-                    var link = document.createElement("a");
-                    link.onclick = button_function;
-                    link.href = ""//Legal, makes page refresh on redirect
+    var link = download_button.getElementsByTagName("a")[0];
+    link.onclick = button_function;
+    link.href = ""//Legal, makes page refresh on redirect
 
-                        var icon = document.createElement("img");
-    
-                        icon.setAttribute("src",button_icon); //todo: custom icon
-                        icon.setAttribute("class","iconlarge activityicon");
-                        icon.setAttribute("alt"," ");
-                        icon.setAttribute("role","presentation");
-                        link.appendChild(icon);
+    var icon = download_button.getElementsByTagName("img")[0];
+    icon.src = button_icon; //todo: custom icon
 
-                        var text = document.createElement("span");
-                        text.setAttribute("class","instancename");
-                            text.appendChild(document.createTextNode(button_text));
-                            var access_hide = document.createElement("span");
-                            access_hide.setAttribute("class","accesshide");
-                            access_hide.appendChild(document.createTextNode("קובץ"));
-                            text.appendChild(access_hide);
-                        link.appendChild(text);
-                    activity_instance.appendChild(link);
-                inner_div.appendChild(activity_instance);
-            div.appendChild(inner_div);
-        download_button.appendChild(div);
+    var text = download_button.getElementsByClassName("instancename")[0];
+    text.textContent = button_text;
+
     return download_button;
 }
